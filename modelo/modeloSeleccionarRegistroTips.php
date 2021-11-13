c0681817241eadbb182f5b29c66ddfd800be1d93
Oscar13XD <hernandezmahechaoscar0@gmail.com>
2021-11-13 16:36:50 -0500

    Registro de Tips 2

--- modelo/modeloSeleccionarRegistroTips.php
+++ modelo/modeloSeleccionarRegistroTips.php
@@ -2,7 +2,7 @@
 require_once __DIR__."/conexion.php";
 class modelo{
     static public function seleccionarRegistroTips($tabla){
-        $stmt = conexion::conectar()->prepare("SELECT IDTip, Titulo, Descripcion, IDUsuario, DATE_FORMAT(Fecha, '%d/%m/%Y') AS Fecha FROM $tabla ORDER BY Fecha DESC, IDTip DESC");
+        $stmt = conexion::conectar()->prepare("SELECT IDTip, Titulo, Descripcion, IDUsuario, DATE_FORMAT(Fecha, '%d/%m/%Y') AS Fecha FROM $tabla ORDER BY MONTH(Fecha) DESC");
         $stmt->execute();
         return $stmt->fetchAll();
         $stmt->close();
